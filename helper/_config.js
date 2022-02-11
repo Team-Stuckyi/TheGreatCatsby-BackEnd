03f78a18c9ddbb598e7f0d1febae9b7aed9a3683
ByeongminLee <bmlee531@gmail.com>
2022-02-11 20:20:54 +0900

    Fix: 서버 파일 정리

--- helper/_config.js
+++ helper/_config.js
@@ -1,17 +1,18 @@
-const path = require("path");
+const path = require('path');
+const keyarr = require('./key.json');
 
 module.exports = {
     // 로그 파일이 저장될 경로 및 출력 레벨
     log: {
         // 개발자가 필요에 의해 기록하는 정보들을 저장할 파일
         debug: {
-            path: path.join(__dirname, "../_files/_logs"),
-            level: "debug",
+            path: path.join(__dirname, '../_files/_logs'),
+            level: 'debug',
         },
         // 시스템에 심각한 문제가 발생했을 떄의 정보를 저장할 파일
         error: {
-            path: path.join(__dirname, "../_files/_logs"),
-            level: "error",
+            path: path.join(__dirname, '../_files/_logs'),
+            level: 'error',
         },
     },
 
@@ -19,26 +20,26 @@ module.exports = {
     server_port: 3000,
 
     /** public 디렉토리 경로 */
-    public_path: path.join(__dirname, "../public"),
+    public_path: path.join(__dirname, '../public'),
 
     /** favicon 경로 */
-    favicon_path: path.join(__dirname, "../public/favicon.png"),
+    favicon_path: path.join(__dirname, '../public/favicon.png'),
 
     /** 쿠키 저장시 사용할 도메인 */
     // 1) localhost인 경우 공백으로 설정
     // 2) 도메인이 naver.com인 경우 앞에 점을 붙여서 명시 ->".naver.com"
-    cookie_domain: "",
+    cookie_domain: '',
 
     /** 보안키 (암호화 키) */
     secure: {
-        cookie_encrypt_key: "1234567890",
-        session_encrypt_key: "1234567890",
+        cookie_encrypt_key: '1234567890',
+        session_encrypt_key: '1234567890',
     },
 
     /** 업로드 경로 */
     upload: {
-        path: "upload",
-        dir: path.join(__dirname, "../_files/upload"),
+        path: 'upload',
+        dir: path.join(__dirname, '../_files/upload'),
         max_size: 1024 * 1024 * 20,
         max_count: 10,
     },
@@ -46,15 +47,14 @@ module.exports = {
     /** 썸네일 이미지 생성 경로 */
     thumbnail: {
         sizes: [480, 750, 1080],
-        dir: path.join(__dirname, "../_files/thumb"),
+        dir: path.join(__dirname, '../_files/thumb'),
     },
 
     /** 데이터베이스 연동 정보 */
     database: {
-        host: "localhost", // mySQL 서버 주소 (다른 PC에 접속인 경우 IP접속)
-        port: 3306, // MySQL 설치시 기본값 3306
-        user: "root", // 접근 권한 아이디 (root = 관리자)
-        password: "root", // 설치시 입력한 비밀번호
-        database: "myschool", // 사용할 데이터베이스 이름
+        host: keyarr.key[0].host, // mySQL 서버 주소 (다른 PC에 접속인 경우 IP접속)
+        user: keyarr.key[2].user, // 접근 권한 아이디 (root = 관리자)
+        password: keyarr.key[3].password, // 설치시 입력한 비밀번호
+        database: keyarr.key[4].database, // 사용할 데이터베이스 이름
     },
 };
--- public/require.js
+++
@@ -1,2 +0,0 @@
-export const axios = require("../node_modules/axios/lib/axios");
-export const FormData = require("../node_modules/form-data/lib/form_data");
