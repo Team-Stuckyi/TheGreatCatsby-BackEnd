0be248646da6b831fe69ce0159ce085e18a9886e
JeonChanMin <cksals3753@naver.com>
2022-03-11 16:19:17 +0900

    Fix: member수정

--- controllers/Members.js
+++ controllers/Members.js
@@ -285,7 +285,7 @@ module.exports = (app) => {
             // 새로 저장된 데이터의 PK값을 활용하여 다시 조회
             const sql2 =
                 "SELECT user_id, name, status, tel, addr1, addr2 FROM members WHERE user_id=?";
-            const [result2] = await dbcon.query(sql2, [user_id]);
+            const [result2] = await dbcon.query(sql2, [result1.insertId]);
 
             // 조회 결과를 미리 준비한 변수에 저장함
             json = result2;
