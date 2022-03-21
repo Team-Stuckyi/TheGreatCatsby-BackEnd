87eaa338431b79a96e019f4f4e9f5bf7f62260f2
Seulgi <dltmfrl600@gmail.com>
2022-03-21 13:39:13 +0900

    Fix: orders/post

--- controllers/Orders.js
+++ controllers/Orders.js
@@ -3,15 +3,15 @@
  */
 
 /** 모듈 참조 부분 */
-const config = require('../helper/_config');
-const logger = require('../helper/LogHelper');
-const regexHelper = require('../helper/RegexHelper');
-const utilHelper = require('../helper/UtilHelper');
-const BadRequestException = require('../exceptions/BadRequestException');
-const RuntimeException = require('../exceptions/RuntimeException');
-const PageNotFoundException = require('../exceptions/PageNotFoundException');
-const router = require('express').Router();
-const mysql2 = require('mysql2/promise');
+const config = require("../helper/_config");
+const logger = require("../helper/LogHelper");
+const regexHelper = require("../helper/RegexHelper");
+const utilHelper = require("../helper/UtilHelper");
+const BadRequestException = require("../exceptions/BadRequestException");
+const RuntimeException = require("../exceptions/RuntimeException");
+const PageNotFoundException = require("../exceptions/PageNotFoundException");
+const router = require("express").Router();
+const mysql2 = require("mysql2/promise");
 
 /** 라우팅 정의 부분 */
 module.exports = (app) => {
@@ -24,21 +24,21 @@ module.exports = (app) => {
      * 전송 정보 : price, order_date, member, order_status => name, member => email, products => name
      */
 
-    router.get('/orders/all', async (req, res, next) => {
+    router.get("/orders/all", async (req, res, next) => {
         //검색어 파라미터 받기 -> 검색어가 없을 경우 전체 목록 조회이므로 유효성 검사 안함
-        const orderIdQuery = req.get('order_id');
-        const orderProdQuery = req.get('name');
-        const orderDateQuery = req.get('order_date');
-        const emailQuery = req.get('email');
-        const orderPriceQuery = req.get('order_price');
-        const orderStatusQuery = req.get('order_status');
+        const orderIdQuery = req.get("order_id");
+        const orderProdQuery = req.get("name");
+        const orderDateQuery = req.get("order_date");
+        const emailQuery = req.get("email");
+        const orderPriceQuery = req.get("order_price");
+        const orderStatusQuery = req.get("order_status");
 
         //현재 페이지 번호 받기 (기본값은 1)
-        const page = req.get('page', 1);
+        const page = req.get("page", 1);
 
         //한 페이지에 보여질 목록 수 받기 (기본값은 10, 최소 10, 최대 30)
-        const rows = req.get('rows', 10);
-        
+        const rows = req.get("rows", 10);
+
         // 데이터 조회 결과가 저장될 빈 변수
         let json = null;
         let pagenation = null;
@@ -58,32 +58,32 @@ module.exports = (app) => {
                 sql1 += " WHERE orders.order_id = ? ";
                 args1.push(orderIdQuery);
             }
-            
+
             if (orderProdQuery != null) {
                 sql1 += " WHERE products.name LIKE concat ('%', ?, '%')";
                 args1.push(orderProdQuery);
             }
-            
+
             if (orderDateQuery != null) {
                 sql1 += " WHERE orders.order_date LIKE concat ('%', ?, '%')";
                 args1.push(orderDateQuery);
             }
-            
+
             if (emailQuery != null) {
                 sql1 += " WHERE members.email = ? ";
                 args1.push(emailQuery);
             }
-            
+
             if (orderPriceQuery != null) {
                 sql1 += " WHERE orders.order_price = ? ";
                 args1.push(orderPriceQuery);
             }
-            
+
             if (orderStatusQuery != null) {
                 sql1 += " WHERE orders.order_status = ? ";
                 args1.push(orderStatusQuery);
             }
-                       
+
             const [result1] = await dbcon.query(sql1, args1);
             totalCount = result1.length;
 
@@ -100,33 +100,33 @@ module.exports = (app) => {
                 sql2 += " WHERE orders.order_id = ? ";
                 args2.push(orderIdQuery);
             }
-            
+
             if (orderProdQuery != null) {
                 sql2 += " WHERE products.name LIKE concat ('%', ?, '%')";
                 args2.push(orderProdQuery);
             }
-            
+
             if (orderDateQuery != null) {
                 sql2 += " WHERE orders.order_date LIKE concat ('%', ?, '%')";
                 args2.push(orderDateQuery);
             }
-            
+
             if (emailQuery != null) {
                 sql2 += " WHERE members.email = ? ";
                 args2.push(emailQuery);
             }
-            
+
             if (orderPriceQuery != null) {
                 sql2 += " WHERE orders.order_price = ? ";
                 args2.push(orderPriceQuery);
             }
-            
+
             if (orderStatusQuery != null) {
                 sql2 += " WHERE orders.order_status = ? ";
                 args2.push(orderStatusQuery);
             }
-            
-            sql2 += ' LIMIT ?,?';
+
+            sql2 += " LIMIT ?,?";
             args2.push(pagenation.offset);
             args2.push(pagenation.listCount);
 
@@ -140,8 +140,18 @@ module.exports = (app) => {
             dbcon.end();
         }
         // 모든 처리에 성공했으므로 정상 조회 결과 구성
-        logger.info(JSON.stringify({ pagenation: pagenation, item: json, totalCount: totalCount }));
-        res.sendJson({ pagenation: pagenation, item: json, totalCount: totalCount });
+        logger.info(
+            JSON.stringify({
+                pagenation: pagenation,
+                item: json,
+                totalCount: totalCount,
+            })
+        );
+        res.sendJson({
+            pagenation: pagenation,
+            item: json,
+            totalCount: totalCount,
+        });
     });
 
     /**
@@ -150,9 +160,9 @@ module.exports = (app) => {
      * 사용자가 리뷰를 작성전에 해당 상품의 리뷰를 썻는지 확인
      * 전송 정보 : order_id, user_id, prod_id
      */
-    router.get('/orders/:user_id/:prod_id', async (req, res, next) => {
-        const userid = req.get('user_id');
-        const prodid = req.get('prod_id');
+    router.get("/orders/:user_id/:prod_id", async (req, res, next) => {
+        const userid = req.get("user_id");
+        const prodid = req.get("prod_id");
 
         if (userid === null || prodid === null) {
             return next(new Error(400));
@@ -168,7 +178,7 @@ module.exports = (app) => {
 
             // 데이터 조회
             const sql =
-                'SELECT order_id, user_id, prod_id FROM orders WHERE user_id = ? && prod_id = ?';
+                "SELECT order_id, user_id, prod_id FROM orders WHERE user_id = ? && prod_id = ?";
             const [result] = await dbcon.query(sql, [userid, prodid]);
 
             // 조회 결과를 미리 준비한 변수에 저장함
@@ -189,8 +199,8 @@ module.exports = (app) => {
      * 전송 정보
      */
 
-    router.delete('/orders/del/:order_id', async (req, res, next) => {
-        const orderid = req.get('order_id');
+    router.delete("/orders/del/:order_id", async (req, res, next) => {
+        const orderid = req.get("order_id");
 
         if (orderid === undefined) {
             return next(new Error(400));
@@ -203,12 +213,12 @@ module.exports = (app) => {
             await dbcon.connect();
 
             // 데이터 삭제하기
-            const sql = 'DELETE FROM orders WHERE order_id=?';
+            const sql = "DELETE FROM orders WHERE order_id=?";
             const [result1] = await dbcon.query(sql, [orderid]);
 
             // 결과 행 수가 0이라면 예외처리
             if (result1.affectedRows < 1) {
-                throw new Error('삭제된 데이터가 없습니다.');
+                throw new Error("삭제된 데이터가 없습니다.");
             }
         } catch (err) {
             return next(err);
@@ -227,26 +237,14 @@ module.exports = (app) => {
      * 전송 정보 : order_price, order_date, order_select, order_count, member => name, member => email, products
      */
 
-    router.post('/orders/post/:userid', async (req, res, next) => {
-        const user_id = req.get("userid");
+    router.post("/orders/post/:userid", async (req, res, next) => {
         // 저장을 위한 파라미터 입력받기
-        const order_price = req.post('order_price');
-        const order_date = req.post('order_date');
-        const order_select = req.post('order_select');
-        const order_count = req.post('order_count');
-        const prodid = req.post('prod_id');
-        const userid = req.post('user_id');
-        // try {
-        //     regexHelper.value(order_price, '총 금액 입력이 없습니다.');
-        //     regexHelper.value(order_select, '결재방식을 고르지 않았습니다.');
-        //     regexHelper.value(order_count, '주문 카운트가 되지 않았습니다.');
-        // } catch (err) {
-        //     return next(err);
-        // }
-        
+        const order_price = req.post("order_price");
+        const order_select = req.post("order_select");
+        const order_count = req.post("order_count");
+        const prodid = req.post("prod_id");
+        const userid = req.post("user_id");
 
-        /** 데이터 저장하기 */
-        // 데이터 조회 결과가 저장될 빈 변수
         let json = null;
 
         try {
@@ -255,26 +253,19 @@ module.exports = (app) => {
             await dbcon.connect();
 
             // 데이터 저장하기
-            const sql1 =
-                "INSERT INTO orders (order_price, order_date, order_select, order_count, prod_id, user_id) VALUES (?, now(), 'T', 'null', ?, ?)";
+            const sql =
+                "INSERT INTO orders (order_price, order_date, order_select, order_count, prod_id, user_id) VALUES (?, now(), ?, ?, ?, ?)";
 
-            const input_data1 = [
+            const input_data = [
                 order_price,
-                order_date,
                 order_select,
                 order_count,
                 prodid,
                 userid,
             ];
-            const [result1] = await dbcon.query(sql1, input_data1);
+            const [result1] = await dbcon.query(sql, input_data);
 
-            // 새로 저장된 데이터의 PK값을 활용하여 다시 조회
-            let sql2 =
-                "SELECT orders.order_id, orders.order_price, DATE_FORMAT(orders.order_date, '%Y-%m-%d') AS order_date, members.name, members.tel, members.addr1, products.name FROM orders INNER JOIN members ON orders.user_id = members.user_id INNER JOIN products ON orders.prod_id = products.prod_id WHERE orders.order_id = ?";
-            const [result2] = await dbcon.query(sql2, [userid]);
-
-            // 조회 결과를 미리 준비한 변수에 저장함
-            json = result2;
+            json = result1;
         } catch (err) {
             return next(err);
         } finally {
@@ -292,8 +283,8 @@ module.exports = (app) => {
      * 전송 정보 : order_price, order_select, order_count
      */
 
-    router.get('/orders/thanku/:order_id', async (req, res, next) => {
-        const orderid = req.get('order_id');
+    router.get("/orders/thanku/:order_id", async (req, res, next) => {
+        const orderid = req.get("order_id");
         /** user_id 값을 가져와서 inner join 한다 */
         if (orderid === null) {
             return next(new Error(400));
@@ -309,7 +300,7 @@ module.exports = (app) => {
 
             // 데이터 조회
             const sql =
-                'SELECT orders.order_price, orders.order_select, orders.order_count, members.user_id, members.tel, members.addr1 FROM orders INNER JOIN members ON orders.user_id = members.user_id WHERE orders.order_id=?';
+                "SELECT orders.order_price, orders.order_select, orders.order_count, members.user_id, members.tel, members.addr1 FROM orders INNER JOIN members ON orders.user_id = members.user_id WHERE orders.order_id=?";
             const [result] = await dbcon.query(sql, [orderid]);
 
             // 조회 결과를 미리 준비한 변수에 저장함
@@ -324,16 +315,16 @@ module.exports = (app) => {
     });
 
     /**
- * PayAdress 컴포넌트
- * 주소를 수정하는 라우터
- * [PUT] /product
- * 전송 정보 : tel, addr1, user_id
- */
+     * PayAdress 컴포넌트
+     * 주소를 수정하는 라우터
+     * [PUT] /product
+     * 전송 정보 : tel, addr1, user_id
+     */
     /** 데이터 수정 --> Update(UPDATE) */
-    router.put('/orders/member/:user_id', async (req, res, next) => {
-        const user_id = req.get('user_id');
-        const tel = req.post('tel');
-        const addr1 = req.post('addr1');
+    router.put("/orders/member/:user_id", async (req, res, next) => {
+        const user_id = req.get("user_id");
+        const tel = req.post("tel");
+        const addr1 = req.post("addr1");
 
         if (user_id === null || tel === null || addr1 === null) {
             return next(new Error(400));
@@ -349,17 +340,17 @@ module.exports = (app) => {
             await dbcon.connect();
 
             // 데이터 수정하기
-            const sql = 'UPDATE members SET tel=?, addr1=? WHERE user_id=?';
+            const sql = "UPDATE members SET tel=?, addr1=? WHERE user_id=?";
             const input_data = [tel, addr1, order_status, user_id];
             const [result1] = await dbcon.query(sql, input_data);
 
             // 결과 행 수가 0이라면 예외처리
             if (result1.affectedRows < 1) {
-                throw new Error('수정된 데이터가 없습니다.');
+                throw new Error("수정된 데이터가 없습니다.");
             }
             // 새로 저장된 데이터의 PK값을 활용하여 다시 조회
             const sql2 =
-                'SELECT members.user_id, members.tel, members.addr1 FROM members INNER JOIN orders ON orders.user_id = members.user_id WHERE members.user_id=?';
+                "SELECT members.user_id, members.tel, members.addr1 FROM members INNER JOIN orders ON orders.user_id = members.user_id WHERE members.user_id=?";
             const [result2] = await dbcon.query(sql2, [user_id]);
 
             // 조회 결과를 미리 준비한 변수에 저장함
@@ -374,7 +365,6 @@ module.exports = (app) => {
         res.sendJson({ item: json });
     });
 
-
     /**
      * 관리자 페이지 - 일반 주문 관리 페이지
      * 주문 정보를 화면에 보여주는 데이터
@@ -382,13 +372,18 @@ module.exports = (app) => {
      * 전송 정보 : order_id, order_date, order_price, order_status
      */
     /** 데이터 수정 --> Update(UPDATE) */
-    router.put('/orders/:order_id', async (req, res, next) => {
-        const order_id = req.get('order_id');
-        const order_date = req.put('order_date');
-        const order_price = req.put('order_price');
-        const order_status = req.put('order_status');
+    router.put("/orders/:order_id", async (req, res, next) => {
+        const order_id = req.get("order_id");
+        const order_date = req.put("order_date");
+        const order_price = req.put("order_price");
+        const order_status = req.put("order_status");
 
-        if (order_id === null || order_date === null || order_price === null || order_status === null) {
+        if (
+            order_id === null ||
+            order_date === null ||
+            order_price === null ||
+            order_status === null
+        ) {
             return next(new Error(400));
         }
 
@@ -402,17 +397,24 @@ module.exports = (app) => {
             await dbcon.connect();
 
             // 데이터 수정하기
-            const sql = 'UPDATE orders SET order_date=?, order_price=?, order_status=? WHERE order_id=?';
-            const input_data = [order_date, order_price, order_status, order_id];
+            const sql =
+                "UPDATE orders SET order_date=?, order_price=?, order_status=? WHERE order_id=?";
+            const input_data = [
+                order_date,
+                order_price,
+                order_status,
+                order_id,
+            ];
             const [result1] = await dbcon.query(sql, input_data);
 
             // 결과 행 수가 0이라면 예외처리
             if (result1.affectedRows < 1) {
-                throw new Error('수정된 데이터가 없습니다.');
+                throw new Error("수정된 데이터가 없습니다.");
             }
 
             // 새로 저장된 데이터의 PK값을 활용하여 다시 조회
-            const sql2 = 'SELECT order_id, order_date, order_price, order_status FROM orders WHERE order_id=?';
+            const sql2 =
+                "SELECT order_id, order_date, order_price, order_status FROM orders WHERE order_id=?";
             const [result2] = await dbcon.query(sql2, [order_id]);
 
             // 조회 결과를 미리 준비한 변수에 저장함
